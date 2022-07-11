88f37ef9d9da966fa34437959978925c70a64bd6
Leonardo Quadros <leonardoq@ciandt.com>
2022-07-11 11:47:24 -0300

    PADRÕES DE PROJETO

---
+++ 0x03/midia_player/AdvancedMediaPlayer.java
@@ -0,0 +1,8 @@
+
+
+public interface AdvancedMediaPlayer {
+
+    void reproduzirVlc(String nomeArquivo);
+
+    void reproduzirMp4(String nomeArquivo);
+}
---
+++ 0x03/midia_player/TipoMedia.java
@@ -0,0 +1,7 @@
+
+
+public enum TipoMedia {
+
+    MP3, VLC, MP4;
+
+}
