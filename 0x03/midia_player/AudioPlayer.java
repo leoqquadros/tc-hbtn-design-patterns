75dfe3815cfdfa0f4908b0a27733096b6a56d567
Leonardo Quadros <leonardoq@ciandt.com>
2022-07-11 12:26:06 -0300

    PADRÕES DE PROJETO

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -5,7 +5,7 @@ public class AudioPlayer implements MediaPlayer{
     @Override
     public void reproduzir(TipoMedia tipoMedia, String nomeArquivo) {
         if (tipoMedia == TipoMedia.MP3)
-            System.out.print("Reproduzindo MP3:" +nomeArquivo);
+            System.out.print("Reproduzindo MP3:" + nomeArquivo);
         else {
             MediaPlayerAdapter mediaPlayerAdapter = new MediaPlayerAdapter();
             mediaPlayerAdapter.reproduzir(tipoMedia,nomeArquivo);
